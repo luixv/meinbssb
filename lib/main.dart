42c95fa15988a4c3291e86fb4403024a5b7d0567
luixv <luismandel@gmail.com>
2025-04-14 12:04:00 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'app.dart'; // Import app.dart to access MyAppWrapper
+import 'app.dart';
 import 'services/api_service.dart';
 import 'services/email_service.dart';
 import 'services/image_service.dart';
