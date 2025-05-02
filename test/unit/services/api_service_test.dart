b2431f39a5ba344327b808a2d8cc0d3bef198e7f
luixv <luismandel@gmail.com>
2025-05-02 11:10:48 +0200

    Update api_service_test.dart

--- test/unit/services/api_service_test.dart
+++ test/unit/services/api_service_test.dart
@@ -7,7 +7,6 @@ import 'package:meinbssb/services/http_client.dart';
 import 'package:meinbssb/services/image_service.dart';
 import 'package:meinbssb/services/network_service.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:http/http.dart' as http;
 import 'api_service_test.mocks.dart';
 
 @GenerateMocks([
@@ -25,7 +24,6 @@ void main() {
   late MockImageService mockImageService;
   late MockCacheService mockCacheService;
   late MockNetworkService mockNetworkService;
-  late MockFlutterSecureStorage mockSecureStorage;
 
   const String testBaseIp = 'test.com';
   const String testPort = '8080';
@@ -36,7 +34,6 @@ void main() {
     mockImageService = MockImageService();
     mockCacheService = MockCacheService();
     mockNetworkService = MockNetworkService();
-    mockSecureStorage = MockFlutterSecureStorage();
 
     apiService = ApiService(
       httpClient: mockHttpClient,
@@ -144,20 +141,59 @@ void main() {
     });
   });
 
-  group('ApiService - fetchPassdaten', () {
+  group('ApiService - fetchPassdatenZVE', () {
     test('should call cacheAndRetrieveData with correct parameters', () async {
       const personId = 123;
+      const passdatenId = 456; // Add the missing passdatenId
       final mockResponse = {
-        'PASSNUMMER': '123',
-        'VEREINNR': '456',
-        'NAMEN': 'Doe',
-        'VORNAME': 'John',
+        'ZVEID': 'ZVE123',
+        'Lizenznummer': 'L456',
       };
 
       when(mockNetworkService.getCacheExpirationDuration())
-          .thenReturn(const Duration(minutes: 5));
+          .thenReturn(const Duration(days: 30));
       when(
-        mockCacheService.cacheAndRetrieveData<Map<String, dynamic>>(
+        mockCacheService.cacheAndRetrieveData<List<dynamic>>(
+          // Note the List<dynamic> here, based on the _map...Response
+          any,
+          any,
+          any,
+          any,
+        ),
+      ).thenAnswer(
+        (_) async => [mockResponse],
+      ); // Mocking a List containing the map
+
+      final result = await apiService.fetchPassdatenZVE(
+        passdatenId,
+        personId,
+      ); // Pass both arguments
+
+      expect(result, [mockResponse]); // Expect a List containing the map
+      verify(
+        mockCacheService.cacheAndRetrieveData<List<dynamic>>(
+          'passdatenzve_${passdatenId}_$personId',
+          any,
+          any,
+          any,
+        ),
+      ).called(1);
+    });
+  });
+
+  group('ApiService - fetchAngemeldeteSchulungen', () {
+    test('should call cacheAndRetrieveData with correct parameters', () async {
+      const personId = 123;
+      const abDatum = '2025-01-01';
+      final mockResponse = [
+        {'Titel': 'Schulung 1', 'Datum': '2025-06-01'},
+        {'Titel': 'Schulung 2', 'Datum': '2025-07-15'},
+      ];
+
+      when(mockNetworkService.getCacheExpirationDuration())
+          .thenReturn(const Duration(hours: 12));
+      when(
+        mockCacheService.cacheAndRetrieveData<List<dynamic>>(
           any,
           any,
           any,
@@ -165,12 +201,80 @@ void main() {
         ),
       ).thenAnswer((_) async => mockResponse);
 
-      final result = await apiService.fetchPassdaten(personId);
+      final result =
+          await apiService.fetchAngemeldeteSchulungen(personId, abDatum);
 
       expect(result, mockResponse);
       verify(
-        mockCacheService.cacheAndRetrieveData<Map<String, dynamic>>(
-          'passdaten_$personId',
+        mockCacheService.cacheAndRetrieveData<List<dynamic>>(
+          'schulungen_$personId',
+          any,
+          any,
+          any,
+        ),
+      ).called(1);
+    });
+  });
+
+  group('ApiService - fetchZweitmitgliedschaften', () {
+    test('should call cacheAndRetrieveData with correct parameters', () async {
+      const personId = 123;
+      final mockResponse = [
+        {'Verein': 'Verein A', 'Sektion': 'Bogen'},
+        {'Verein': 'Verein C', 'Sektion': 'Gewehr'},
+      ];
+
+      when(mockNetworkService.getCacheExpirationDuration())
+          .thenReturn(const Duration(days: 7));
+      when(
+        mockCacheService.cacheAndRetrieveData<List<dynamic>>(
+          any,
+          any,
+          any,
+          any,
+        ),
+      ).thenAnswer((_) async => mockResponse);
+
+      final result = await apiService.fetchZweitmitgliedschaften(personId);
+
+      expect(result, mockResponse);
+      verify(
+        mockCacheService.cacheAndRetrieveData<List<dynamic>>(
+          'zweitmitgliedschaften_$personId',
+          any,
+          any,
+          any,
+        ),
+      ).called(1);
+    });
+  });
+
+  group('ApiService - fetchPassdatenZVE', () {
+    test('should call cacheAndRetrieveData with correct parameters', () async {
+      const personId = 123;
+      const passdatenId = 456;
+      final mockResponse = {
+        'ZVEID': 'ZVE123',
+        'Lizenznummer': 'L456',
+      };
+
+      when(mockNetworkService.getCacheExpirationDuration())
+          .thenReturn(const Duration(days: 30));
+      when(
+        mockCacheService.cacheAndRetrieveData<List<dynamic>>(
+          any,
+          any,
+          any,
+          any,
+        ),
+      ).thenAnswer((_) async => [mockResponse]);
+
+      final result = await apiService.fetchPassdatenZVE(passdatenId, personId);
+
+      expect(result, [mockResponse]);
+      verify(
+        mockCacheService.cacheAndRetrieveData<List<dynamic>>(
+          'passdatenzve_${passdatenId}_$personId',
           any,
           any,
           any,
