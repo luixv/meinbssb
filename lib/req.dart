5db749ee5304d17fff0929bed73c1644dbeef714
luixv <luismandel@gmail.com>
2025-05-16 11:09:41 +0200

    debugPrint

---
+++ lib/req.dart
@@ -0,0 +1,57 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:logging/logging.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  // Define the URL
+  Logger.root.level = Level.ALL;
+  const String url =
+      'https://webintern.bssb.bayern:56400/rest/zmi/api/LoginMyBSSB';
+
+  // Define the body
+
+  final Map<String, String> body = {
+    'Email': 'kostas@rizoudis1.de',
+    'Passwort': 'test1',
+  };
+  final String jsonBody = jsonEncode(body);
+  debugPrint(jsonBody);
+
+  String cookieValue =
+      'access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJSb2xlcyI6InN0YW5kYXJkLHdlYix3aWVzbix3ZWJCYXVrYXN0ZW4sYWNjZXNzIiwiZHVyYXRpb24iOiIxODk5LTEyLTMxIiwiaXNzIjoiTUFSUy1DdXJpb3NpdHkiLCJleHAiOjE3NDczOTQ3ODMsImlhdCI6MTc0NzMwODM4MywiVXNlck5hbWUiOiJ3ZWJVc2VyIiwiQkVOVVRaRVJJRCI6Nn0.nyrknugJrTj77N1xPyA4NQLjrK2Zn89qBgKl_VErL5U';
+
+  const String authorization =
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJSb2xlcyI6InN0YW5kYXJkLHdlYix3aWVzbix3ZWJCYXVrYXN0ZW4sYWNjZXNzIiwiZHVyYXRpb24iOiIxODk5LTEyLTMxIiwiaXNzIjoiTUFSUy1DdXJpb3NpdHkiLCJleHAiOjE3NDczOTQ3ODMsImlhdCI6MTc0NzMwODM4MywiVXNlck5hbWUiOiJ3ZWJVc2VyIiwiQkVOVVRaRVJJRCI6Nn0.nyrknugJrTj77N1xPyA4NQLjrK2Zn89qBgKl_VErL5U';
+
+  final Map<String, String> headers = {
+    'Content-Type': 'application/json',
+    'Content-Length': utf8.encode(jsonBody).length.toString(),
+    'Host': 'webintern.bssb.bayern:56400',
+    'Cookie': cookieValue,
+    'Authorization': authorization,
+  };
+
+  debugPrint('Sending GET Request to: $url');
+  debugPrint('Headers: $headers');
+  debugPrint('Body: $jsonBody');
+
+  try {
+    final request = http.Request('GET', Uri.parse(url));
+    request.headers.addAll(headers);
+    request.body = jsonBody;
+
+    final http.StreamedResponse streamedResponse =
+        await http.Client().send(request);
+
+    final http.Response response =
+        await http.Response.fromStream(streamedResponse);
+
+    debugPrint('Response Status Code: ${response.statusCode}');
+    debugPrint('Response body: ${response.body}');
+  } catch (e) {
+    debugPrint('GET Request Error: $e');
+  } finally {
+    http.Client().close();
+  }
+}
