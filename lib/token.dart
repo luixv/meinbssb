f9a48b0607471075e19d09f1fb394ced2d229f45
luixv <luismandel@gmail.com>
2025-05-16 15:02:08 +0200

    Update token.dart

--- lib/token.dart
+++ lib/token.dart
@@ -1,44 +1,38 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 void main() async {
   const String tokenServerURL =
       'https://webintern.bssb.bayern:56400/rest/zmi/token';
 
-  final Map<String, String> body = {
+  final Map<String, String> formFields = {
     'username': 'webuser',
     'password':
         '8702973BCE08DB9DC590BF3CDBBA1873E8B7C7296CB641F7012E123041AE4CC3',
   };
-  final String jsonBody = jsonEncode(body);
-  print(jsonBody);
 
-  final Map<String, String> headers = {
-    'Content-Type': 'application/json',
-    'Content-Length': utf8.encode(jsonBody).length.toString(),
-    'Host': 'webintern.bssb.bayern:56400',
-  };
+  // Create a multipart request.
+  var request = http.MultipartRequest('POST', Uri.parse(tokenServerURL));
 
-  print('Sending GET Request to: $tokenServerURL');
-  print('Headers: $headers');
-  print('Body: $jsonBody');
+  // Add the form fields to the request.
+  formFields.forEach((key, value) {
+    request.fields[key] = value;
+  });
 
+  //Log
+  print("url: ${request.url}");
+  print("fields: ${request.fields}");
+
+  // Send the request.
   try {
-    final request = http.Request('GET', Uri.parse(tokenServerURL));
-    request.headers.addAll(headers);
-    request.body = jsonBody;
-
-    final http.StreamedResponse streamedResponse =
-        await http.Client().send(request);
-
+    final http.StreamedResponse streamedResponse = await request.send();
     final http.Response response =
         await http.Response.fromStream(streamedResponse);
 
     print('Response Status Code: ${response.statusCode}');
     print('Response body: ${response.body}');
   } catch (e) {
-    print('GET Request Error: $e');
+    print('POST Request Error: $e'); // Changed to POST
   } finally {
-    http.Client().close();
+    // No need to close the client here, as we are not creating one.
   }
 }
