6a85f8579e3b0c69f292cfa1d23c38cd9779b860
luixv <luismandel@gmail.com>
2025-05-21 14:32:32 +0200

    webuser != webUser... you know

--- lib/token.dart
+++ lib/token.dart
@@ -12,15 +12,13 @@ void main() async {
       'https://webintern.bssb.bayern:56400/rest/zmi/token';
 
   final Map<String, String> formFields = {
-    'username': 'webuser',
+    'username': 'webUser',
     'password':
         '8702973BCE08DB9DC590BF3CDBBA1873E8B7C7296CB641F7012E123041AE4CC3',
   };
 
   // Create a multipart request.
   var request = http.MultipartRequest('POST', Uri.parse(tokenServerURL));
-  request.headers['Cookie'] =
-      'access_token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJSb2xlcyI6InN0YW5kYXJkLHdlYix3aWVzbix3ZWJCYXVrYXN0ZW4sYWNjZXNzIiwiZHVyYXRpb24iOiIxODk5LTEyLTMxIiwiaXNzIjoiTUFSUy1DdXJpb3NpdHkiLCJleHAiOjE3NDc5MDg5OTQsImlhdCI6MTc0NzgyMjU5NCwiVXNlck5hbWUiOiJ3ZWJVc2VyIiwiQkVOVVRaRVJJRCI6Nn0.fmwEbE_uVJ2773zBJ0Qwru41bPjJlqhkrRb2B71ENLE';
 
   // Add the form fields to the request.
   formFields.forEach((key, value) {
@@ -39,6 +37,19 @@ void main() async {
 
     logger.fine('Response Status Code: ${response.statusCode}');
     logger.fine('Response body: ${response.body}');
+
+    logger.fine('==== Response Headers ====');
+    response.headers.forEach((key, value) {
+      logger.fine('$key: $value'); // Log each header line
+    });
+
+    // Optional: Extract cookies separately (if 'set-cookie' exists)
+    if (response.headers.containsKey('set-cookie')) {
+      logger.fine('\n==== Cookies (from set-cookie) ====');
+      response.headers['set-cookie']!.split(',').forEach((cookie) {
+        logger.fine(cookie.trim());
+      });
+    }
   } catch (e) {
     logger.fine('POST Request Error: $e');
   }
