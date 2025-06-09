3e1e8274f49e016f21051a1ecef50c3d9096ff36
luis <luismandel@gmail.com>
2025-06-09 12:47:49 +0200

    Update ui_constants.dart

--- lib/constants/ui_constants.dart
+++ lib/constants/ui_constants.dart
@@ -5,28 +5,32 @@
 import 'package:flutter/material.dart';
 
 class UIConstants {
-  // Colors "apiBackground": "e2f0d9",
-
-  static const Color defaultAppColor = Color(0xFF006400); // Main green color
+  // Colors
+  static const Color primaryColor = Color(0xFF006400); // Main green color
   static const Color backgroundColor = Color(0xFFe2f0d9);
   static const Color foregroundColor = Colors.lightGreen;
+  static const Color textColor = Colors.black;
+  static const Color errorColor = Colors.red;
+  static const Color successColor = Colors.green;
+  static const Color warningColor = Color(0xFFFFA000);
+  static const Color infoColor = Color(0xFF2196F3);
+  static const Color linkColor = Colors.lightGreen;
+  static const Color greyColor = Colors.grey;
+  static const Color whiteColor = Colors.white;
 
-  // Button Properties
+  // Button Colors
   static const Color cancelButtonBackground = Colors.lightGreen;
-  static const Color deleteButtonBackground = defaultAppColor;
-
   static const Color acceptButtonBackground = Colors.white;
-  static const Color deletetButtonBackground = Colors.white;
+  static const Color deleteButtonBackground = primaryColor;
   static const Color submitButtonBackground = Colors.lightGreen;
 
-  static const Color submitButtonText = Colors.white;
+  static const Color buttonTextColor = Colors.white;
   static const Color cancelButtonText = Colors.white;
   static const Color deleteButtonText = Colors.white;
+  static const Color submitButtonText = Colors.white;
 
-  static const Color disabledSubmitButtonText = Colors.white;
-
-  // Icon Properties
-  static const Color deleteIcon = defaultAppColor;
+  // Icon Colors
+  static const Color deleteIcon = primaryColor;
   static const Color closeIcon = Colors.white;
   static const Color checkIcon = Colors.white;
   static const Color addIcon = Colors.white;
@@ -35,53 +39,36 @@ class UIConstants {
   static const Color noConnectivityIcon = Colors.red;
   static const Color bluetoothConnected = Colors.grey;
   static const Color networkCheck = Colors.grey;
-
   static const Color circularProgressIndicator = Colors.white;
+
+  // Card Colors
   static const Color cardColor = Colors.white;
+  static const Color boxDecorationColor = Colors.white;
+  static const Color cursorColor = Colors.black;
 
-  static const Color primarySwatch = Colors.blue;
-  static const Color greySubtitleText = Colors.grey;
-
-//Selection Properties
+  // Selection Colors
   static const Color selectionColor = Colors.transparent;
   static const Color selectionHandleColor = Colors.transparent;
   static const Color highlightColor = Colors.transparent;
   static const Color splashColor = Colors.transparent;
 
-// Errors Properties
-  static const Color error = Colors.red;
-  static const Color success = Colors.green;
-
-// Cookies Dialog color
-  static const Color cookiesDialogColor = Colors.transparent;
-
-// Link Color
-  static const Color linkColor = Colors.lightGreen;
-
-// News Properties
-  static const Color news = Color(0xFFF4B183);
-  static const Color newsText = Colors.white;
-
-//Table Properties
+  // Table Colors
   static const Color tableBackground = Colors.white;
   static const Color tableBorder = Colors.white;
   static const Color tableContentColor = Colors.black;
   static const Color tileColor = Colors.white;
 
-  static const Color boxDecorationColor = Colors.white;
-  static const Color cursorColor = Colors.black;
-
-  // Calendar Properties
+  // Calendar Colors
   static const Color calendarBackgroundColor = backgroundColor;
   static const Color calendarHeaderColor = Colors.lightGreen;
-  static const Color calendarHeaderTextColor = defaultAppColor;
+  static const Color calendarHeaderTextColor = primaryColor;
   static const Color calendarTextColor = Colors.black;
   static const Color calendarTodayColor = Colors.white;
   static const Color calendarTodayTextColor = Colors.black;
   static const Color calendarWeekendColor = Colors.white;
   static const Color calendarWeekendTextColor = Colors.black;
   static const Color calendarSelectedTextColor = Colors.white;
-  static const Color calendarSelectedBackgroundColor = defaultAppColor;
+  static const Color calendarSelectedBackgroundColor = primaryColor;
   static const Color calendarDisabledTextColor = Colors.grey;
   static const Color calendarDisabledBackgroundColor = Colors.white;
   static const Color calendarDisabledSelectedTextColor = Colors.white;
@@ -92,271 +79,16 @@ class UIConstants {
   static const Color calendarWeekendBorderColor = Colors.black;
   static const Color calendarDisabledBorderColor = Colors.grey;
   static const Color calendarDisabledSelectedBorderColor = Colors.grey;
-  static const Color calendarHeaderTextBorderColor = Colors.black;
-  static const Color calendarTextBorderColor = Colors.black;
-  static const Color calendarTodayTextBorderColor = Colors.black;
-  static const Color calendarWeekendTextBorderColor = Colors.black;
-  static const Color calendarSelectedTextBorderColor = Colors.white;
-  static const Color calendarSelectedBackgroundBorderColor = defaultAppColor;
-  static const Color calendarDisabledTextBorderColor = Colors.grey;
-  static const Color calendarDisabledBackgroundBorderColor = Colors.white;
-  static const Color calendarDisabledSelectedTextBorderColor = Colors.white;
-  static const Color calendarDisabledSelectedBackgroundBorderColor =
-      Colors.grey;
-  static const Color calendarHeaderTextColorBorder = Colors.black;
-  static const Color calendarTextColorBorder = Colors.black;
 
-  static const Color calendarColor = Colors.black;
-  static const Color calendarSelectedColor = defaultAppColor;
-
-  // Font Properties  Arial, Calibri, OpenSans, Roboto (no more)
-  static const String defaultFontFamily = 'OpenSans'; //
+  // Font Properties
+  static const String defaultFontFamily = 'OpenSans';
 
   // Font Sizes
-  static const double headerFontSize = 24.0; // "Hier Registrieren"
-  static const double bodyFontSize = 14.0; // Normal text, form fields
-  static const double titleFontSize = 20.0; // AppBar title
-  static const double subtitleFontSize = 16.0; // Form labels
-
-  // Spacing and Padding
-  static const double defaultSpacing = 20.0; // Major section spacing
-  static const double smallSpacing = 8.0; // Minor element spacing
-  static const double mediumSpacing = 10.0; // Medium element spacing
-  static const double largeSpacing = 12.0; // large element spacing
-  static const double largerSpacing = 16.0; // larger element spacing
-  static const double defaultPadding = 16.0; // Standard padding
-
-  static const double topPadding = 60.0; // Top padding in scroll view
-
-  static const EdgeInsets buttonPadding = EdgeInsets.symmetric(vertical: 16.0);
-
-  // Screen Padding
-  static const EdgeInsets screenPadding = EdgeInsets.fromLTRB(
-    defaultPadding, // left
-    topPadding, // top
-    defaultPadding, // right
-    defaultPadding, // bottom
-  );
-
-  // Logo Size
-  static const double logoSize = 100.0;
-  static const double cornerRadius = 8.0;
-  static const int fabHeight = 16;
-
-  // Additional UI Constants
-  static const double defaultStrokeWidth = 2.0;
-  static const double defaultIconWidth = 60.0;
-  static const double defaultButtonWidth = 120.0;
-  static const double defaultImageHeight = 100.0;
-  static const double defaultHorizontalSpacing = 8.0;
-  static const double defaultHorizontalPadding = 16.0;
-  static const double defaultSeparatorHeight = 10.0;
-
-  static const double defaultIconSize = 16.0;
-
-  // Text Styles
-  static const TextStyle headerStyle = TextStyle(
-    fontSize: headerFontSize,
-    fontFamily: defaultFontFamily,
-    fontWeight: FontWeight.bold,
-  );
-
-  static const TextStyle dialogTitleStyle = TextStyle(
-    color: defaultAppColor,
-    fontWeight: FontWeight.bold,
-  );
-
-  static const TextStyle dialogContentStyle = TextStyle(
-    fontSize: subtitleFontSize,
-    color: tableContentColor,
-  );
-
-  static const TextStyle dialogBoldContentStyle = TextStyle(
-    fontWeight: FontWeight.bold,
-    color: tableContentColor,
-  );
-
-  static const TextStyle sectionTitleStyle = TextStyle(
-    fontSize: subtitleFontSize,
-    fontWeight: FontWeight.bold,
-    color: defaultAppColor,
-  );
-
-  static const TextStyle formLabelStyle = TextStyle(
-    fontSize: subtitleFontSize,
-    color: tableContentColor,
-  );
-
-  static const TextStyle formValueStyle = TextStyle(
-    fontWeight: FontWeight.bold,
-    fontSize: bodyFontSize,
-  );
-
-  static const TextStyle appBarTitleStyle = TextStyle(
-    fontSize: titleFontSize,
-    fontWeight: FontWeight.bold,
-    color: defaultAppColor,
-  );
-
-  static const TextStyle listItemTitleStyle = TextStyle(
-    fontSize: subtitleFontSize,
-    fontWeight: FontWeight.bold,
-    color: tableContentColor,
-  );
-
-  static const TextStyle listItemSubtitleStyle = TextStyle(
-    fontSize: bodyFontSize,
-    color: greySubtitleText,
-  );
-
-  static const TextStyle errorMessageStyle = TextStyle(
-    fontSize: bodyFontSize,
-    color: error,
-  );
-
-  static const TextStyle successMessageStyle = TextStyle(
-    fontSize: bodyFontSize,
-    color: success,
-  );
-
-  static const TextStyle offlineMessageStyle = TextStyle(
-    fontSize: subtitleFontSize,
-    color: greySubtitleText,
-  );
-
-  static const TextStyle subtitleStyle = TextStyle(
-    fontSize: 18.0,
-    fontWeight: FontWeight.w600,
-    fontFamily: defaultFontFamily,
-    color: Colors.black87,
-  );
-
-  static const TextStyle titleStyle = TextStyle(
-    fontSize: titleFontSize,
-    fontFamily: defaultFontFamily,
-    fontWeight: FontWeight.bold,
-    color: Colors.black,
-    backgroundColor: Colors.transparent,
-    decorationColor: Colors.transparent,
-  );
-
-  static const TextStyle bodyStyle = TextStyle(
-    fontSize: bodyFontSize,
-    fontFamily: defaultFontFamily,
-    color: Colors.black,
-    fontWeight: FontWeight.normal,
-    backgroundColor: Colors.transparent,
-    decorationColor: Colors.transparent,
-  );
-
-  static const TextStyle newsStyle = TextStyle(
-    fontSize: 16.0,
-    fontFamily: defaultFontFamily,
-    color: newsText,
-    fontWeight: FontWeight.bold,
-    backgroundColor: news,
-    decorationColor: Colors.transparent,
-  );
-
-  static const TextStyle linkStyle = TextStyle(
-    fontSize: bodyFontSize,
-    fontFamily: defaultFontFamily,
-    decoration: TextDecoration.underline,
-    fontWeight: FontWeight.normal,
-  );
-
-  static const TextStyle successStyle = TextStyle(
-    fontSize: bodyFontSize,
-    fontFamily: defaultFontFamily,
-    color: Colors.green,
-  );
-
-  static const TextStyle errorStyle = TextStyle(
-    fontSize: bodyFontSize,
-    fontFamily: defaultFontFamily,
-    color: Colors.red,
-  );
-
-  // Form Styles
-  static const InputDecoration defaultInputDecoration = InputDecoration(
-    contentPadding: EdgeInsets.symmetric(
-      vertical: smallSpacing,
-      horizontal: defaultPadding,
-    ),
-    border: OutlineInputBorder(),
-    errorStyle: errorStyle,
-  );
-
-  // Duration Constants
-  static const Duration snackBarDuration = Duration(seconds: 5);
-  static const Duration loadingDelay = Duration(milliseconds: 100);
-
-  // Button Styles
-  static final ButtonStyle cancelButtonStyle = ElevatedButton.styleFrom(
-    backgroundColor: cancelButtonBackground,
-    padding: buttonPadding,
-  );
-
-  static final ButtonStyle deleteButtonStyle = ElevatedButton.styleFrom(
-    backgroundColor: deleteButtonBackground,
-    padding: buttonPadding,
-  );
-
-  static final ButtonStyle acceptButtonStyle = ElevatedButton.styleFrom(
-    backgroundColor: acceptButtonBackground,
-    padding: buttonPadding,
-  );
-
-  static final ButtonStyle submitButtonStyle = ElevatedButton.styleFrom(
-    backgroundColor: submitButtonBackground,
-    padding: buttonPadding,
-  );
-
-  // Icon Styles
-  static const IconThemeData defaultIconTheme = IconThemeData(
-    color: Colors.white,
-    size: defaultIconSize,
-  );
-
-  static const IconThemeData errorIconTheme = IconThemeData(
-    color: error,
-    size: defaultIconSize,
-  );
-
-  static const IconThemeData successIconTheme = IconThemeData(
-    color: success,
-    size: defaultIconSize,
-  );
-
-  // Input Decoration Styles
-  static const InputDecoration formInputDecoration = InputDecoration(
-    contentPadding: EdgeInsets.symmetric(
-      vertical: smallSpacing,
-      horizontal: defaultPadding,
-    ),
-    border: OutlineInputBorder(),
-    errorStyle: errorStyle,
-    filled: true,
-    fillColor: backgroundColor,
-  );
-
-  // Card Styles
-  static final CardTheme defaultCardTheme = CardTheme(
-    color: cardColor,
-    elevation: 2.0,
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(cornerRadius),
-    ),
-  );
-
-  // List Tile Styles
-  static final ListTileThemeData defaultListTileTheme = ListTileThemeData(
-    tileColor: tileColor,
-    contentPadding: EdgeInsets.symmetric(
-      horizontal: defaultPadding,
-      vertical: smallSpacing,
-    ),
-  );
+  static const double headerFontSize = 24.0;
+  static const double bodyFontSize = 14.0;
+  static const double titleFontSize = 20.0;
+  static const double subtitleFontSize = 16.0;
+  static const double buttonFontSize = 16.0;
 
   // Spacing
   static const double spacingXS = 4.0;
@@ -364,38 +96,6 @@ class UIConstants {
   static const double spacingM = 16.0;
   static const double spacingL = 24.0;
   static const double spacingXL = 32.0;
-
-  // Colors
-  static const Color primaryColor = defaultAppColor;
-  static const Color textColor = tableContentColor;
-  static const Color errorColor = Color(0xFFD32F2F);
-  static const Color successColor = Color(0xFF4CAF50);
-  static const Color warningColor = Color(0xFFFFA000);
-  static const Color infoColor = Color(0xFF2196F3);
-
-  // Dialog Styles
-  static final dialogTitleStyle = TextStyle(
-    fontSize: titleFontSize,
-    fontWeight: FontWeight.bold,
-    color: textColor,
-  );
-
-  static final dialogContentStyle = TextStyle(
-    fontSize: bodyFontSize,
-    color: textColor,
-  );
-
-  static final dialogButtonStyle = TextStyle(
-    fontSize: buttonFontSize,
-    fontWeight: FontWeight.bold,
-    color: primaryColor,
-  );
-
-  static const dialogPadding = EdgeInsets.all(spacingM);
-  static const dialogButtonPadding = EdgeInsets.symmetric(
-    horizontal: spacingM,
-    vertical: spacingS,
-  );
 
   // Common Widgets
   static const SizedBox spacingXS = SizedBox(width: spacingXS, height: spacingXS);
@@ -416,20 +116,76 @@ class UIConstants {
   static const SizedBox verticalSpacingL = SizedBox(height: spacingL);
   static const SizedBox verticalSpacingXL = SizedBox(height: spacingXL);
 
-  // Dialog Button Styles
-  static final dialogAcceptButtonStyle = ElevatedButton.styleFrom(
-    backgroundColor: acceptButtonBackground,
-    padding: buttonPadding,
+  // Padding
+  static const EdgeInsets defaultPadding = EdgeInsets.all(spacingM);
+  static const EdgeInsets defaultHorizontalPadding = EdgeInsets.symmetric(horizontal: spacingM);
+  static const EdgeInsets defaultVerticalPadding = EdgeInsets.symmetric(vertical: spacingM);
+  static const EdgeInsets buttonPadding = EdgeInsets.symmetric(vertical: spacingM);
+  static const EdgeInsets screenPadding = EdgeInsets.fromLTRB(
+    spacingM,
+    60.0,
+    spacingM,
+    spacingM,
   );
 
-  static final dialogCancelButtonStyle = ElevatedButton.styleFrom(
-    backgroundColor: cancelButtonBackground,
-    padding: buttonPadding,
+  // Sizes
+  static const double logoSize = 100.0;
+  static const double cornerRadius = 8.0;
+  static const double fabHeight = 16.0;
+  static const double defaultStrokeWidth = 2.0;
+  static const double defaultIconSize = 16.0;
+  static const double defaultIconWidth = 60.0;
+  static const double defaultButtonWidth = 120.0;
+  static const double defaultImageHeight = 100.0;
+  static const double defaultSeparatorHeight = 10.0;
+
+  // Text Styles
+  static final titleStyle = TextStyle(
+    fontSize: titleFontSize,
+    fontFamily: defaultFontFamily,
+    fontWeight: FontWeight.bold,
+    color: textColor,
+  );
+
+  static final subtitleStyle = TextStyle(
+    fontSize: subtitleFontSize,
+    fontFamily: defaultFontFamily,
+    fontWeight: FontWeight.w600,
+    color: textColor,
+  );
+
+  static final bodyStyle = TextStyle(
+    fontSize: bodyFontSize,
+    fontFamily: defaultFontFamily,
+    color: textColor,
+  );
+
+  static final headerStyle = TextStyle(
+    fontSize: headerFontSize,
+    fontFamily: defaultFontFamily,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final appBarTitleStyle = TextStyle(
+    fontSize: titleFontSize,
+    fontWeight: FontWeight.bold,
+    color: primaryColor,
+  );
+
+  static final listItemTitleStyle = TextStyle(
+    fontSize: subtitleFontSize,
+    fontWeight: FontWeight.bold,
+    color: tableContentColor,
+  );
+
+  static final listItemSubtitleStyle = TextStyle(
+    fontSize: bodyFontSize,
+    color: greyColor,
   );
 
   // Form Styles
   static final formLabelStyle = TextStyle(
-    fontSize: bodyFontSize,
+    fontSize: subtitleFontSize,
     color: textColor,
   );
 
@@ -471,7 +227,65 @@ class UIConstants {
   );
 
   static final offlineMessageStyle = TextStyle(
+    fontSize: subtitleFontSize,
+    color: greyColor,
+  );
+
+  // Dialog Styles
+  static final dialogTitleStyle = TextStyle(
+    fontSize: titleFontSize,
+    fontWeight: FontWeight.bold,
+    color: textColor,
+  );
+
+  static final dialogContentStyle = TextStyle(
     fontSize: bodyFontSize,
-    color: warningColor,
+    color: textColor,
+  );
+
+  static final dialogButtonStyle = TextStyle(
+    fontSize: buttonFontSize,
+    fontWeight: FontWeight.bold,
+    color: primaryColor,
+  );
+
+  static const dialogPadding = EdgeInsets.all(spacingM);
+  static const dialogButtonPadding = EdgeInsets.symmetric(
+    horizontal: spacingM,
+    vertical: spacingS,
+  );
+
+  // Dialog Button Styles
+  static final dialogAcceptButtonStyle = ElevatedButton.styleFrom(
+    backgroundColor: acceptButtonBackground,
+    padding: buttonPadding,
+  );
+
+  static final dialogCancelButtonStyle = ElevatedButton.styleFrom(
+    backgroundColor: cancelButtonBackground,
+    padding: buttonPadding,
+  );
+
+  // Button Styles
+  static final primaryButtonStyle = ElevatedButton.styleFrom(
+    backgroundColor: primaryColor,
+    padding: buttonPadding,
+  );
+
+  static final secondaryButtonStyle = ElevatedButton.styleFrom(
+    backgroundColor: backgroundColor,
+    padding: buttonPadding,
+  );
+
+  // Input Decoration
+  static final formInputDecoration = InputDecoration(
+    labelStyle: formLabelStyle,
+    hintStyle: formLabelStyle,
+    errorStyle: formErrorStyle,
+    border: const OutlineInputBorder(),
+    contentPadding: const EdgeInsets.symmetric(
+      horizontal: spacingM,
+      vertical: spacingS,
+    ),
   );
 }
