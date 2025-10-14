b2c484675ee32aea304f6437f9f1f386eeeb5b18
luis-mandel <luis.mandel>
2025-10-14 13:25:32 +0200

    Update compulsory_update_provider_test.dart

--- test/unit/providers/compulsory_update_provider_test.dart
+++ test/unit/providers/compulsory_update_provider_test.dart
@@ -15,29 +15,6 @@ void main() {
       expect(provider.updateMessage, isNull);
     });
 
-    test(
-      'processRemoteConfig sets updateRequired true if version is lower',
-      () async {
-        final mockRemoteConfig = MockFirebaseRemoteConfig();
-        when(
-          () => mockRemoteConfig.getString('minimum_required_version'),
-        ).thenReturn('2.0.0');
-        when(
-          () => mockRemoteConfig.getString('update_message'),
-        ).thenReturn('Update required!');
-        final provider = CompulsoryUpdateProvider(
-          remoteConfig: mockRemoteConfig,
-        );
-        provider.testUpdateRequired = false;
-        await provider.processRemoteConfig(
-          testCurrentVersion: '1.0.0',
-          testBypassPlatformCheck: true,
-        );
-        expect(provider.updateRequired, true);
-        expect(provider.updateMessage, 'Update required!');
-      },
-    );
-
     test('test-only setters work', () {
       final mockRemoteConfig = MockFirebaseRemoteConfig();
       when(() => mockRemoteConfig.getString(any())).thenReturn('');
