6300c3741410b00f06aa6d2bfc0579c9ded86948
luis-mandel <luis.mandel>
2025-10-14 12:50:35 +0200

    Compulsory update layout

--- lib/widgets/compulsory_update_gate.dart
+++ lib/widgets/compulsory_update_gate.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:meinbssb/constants/ui_constants.dart';
 import '../providers/compulsory_update_provider.dart';
 
 class CompulsoryUpdateGate extends StatelessWidget {
@@ -16,12 +17,28 @@ class CompulsoryUpdateGate extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const Icon(Icons.system_update, size: 64, color: Colors.orange),
+                SizedBox(height: 40),
+                Image.asset(
+                  'assets/images/BSSB_Wappen.png',
+                  width: UIConstants.logoSize * 1.2,
+                  height: UIConstants.logoSize * 1.2,
+                ),
+                const SizedBox(
+                  height: 120,
+                ), // Even more space between logo and icon
+                const Icon(
+                  Icons.system_update,
+                  size: 64,
+                  color: UIConstants.defaultAppColor,
+                ),
                 const SizedBox(height: 24),
                 Text(
                   compulsoryUpdate.updateMessage ??
                       'Um fortzufahren, müssen Sie das neue Update installieren.',
-                  style: const TextStyle(fontSize: 20, color: Colors.orange),
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: UIConstants.defaultAppColor,
+                  ),
                   textAlign: TextAlign.center,
                 ),
               ],
